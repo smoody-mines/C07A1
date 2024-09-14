1805cf23a984030abfb8c5d378d065921b530d09
James Slopey <slopey@mines.edu>
2024-09-13 18:16:22 -0600

    Implemented Sitter class to test LevelSetup

---
+++ src/levelPieces/Sitter.java
@@ -0,0 +1,14 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Sitter extends GamePiece implements Drawable {
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
