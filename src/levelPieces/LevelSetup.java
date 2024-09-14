cde262e7883ffbca4c9588700089c1076631a82c
James Slopey <slopey@mines.edu>
2024-09-13 18:16:42 -0600

    Implemented Sitter class to test LevelSetup

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -1,5 +1,31 @@
 package levelPieces;
 
+import gameEngine.Drawable;
+
 public class LevelSetup {
+	
+	
+	private Drawable[] board;
+	
+	public void createLevel(int levelNum){
+		
+		if (levelNum == 1) {level1();}
+		
+		else if(levelNum == 2) {level2();}
+		
+		else {
+			System.out.println("Recieved Level not 1 or 2");
+		}
+	
+	}
+	
+	private void level1(){
+		board = new Drawable[BOARD_SIZE];
+	}
+	
+	private void level2(){
+		
+	}
+
 
 }
--- src/levelPieces/Sitter.java
+++ src/levelPieces/Sitter.java
@@ -4,11 +4,15 @@ import gameEngine.Drawable;
 import gameEngine.InteractionResult;
 
 public class Sitter extends GamePiece implements Drawable {
+	
+	public Sitter(int location) {
+		super('S', "Sitter", location);
+	}
 
 	@Override
 	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
 		// TODO Auto-generated method stub
-		return null;
+		return InteractionResult.ADVANCE;
 	}
 
 }
